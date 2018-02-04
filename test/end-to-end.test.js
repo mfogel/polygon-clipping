1d438fb609baf22b62a549183958f38a08e71e16
Mike Fogel <mike@fogel.ca>
2018-02-04 10:02:23 -0300

    End-to-end testing framework
    Includes a starter test

---
+++ test/end-to-end.test.js
@@ -0,0 +1,47 @@
+/* eslint-env jest */
+
+const fs = require('fs')
+const path = require('path')
+const load = require('load-json-file')
+const polygonClipping = require('../src')
+
+const endToEndDir = 'test/end-to-end'
+const possibleOps = ['union', 'intersection', 'diff', 'xor']
+
+/** USE ME TO ONLY RUN ONE TEST **/
+const targetOnly = null
+const opOnly = null
+
+describe('end to end', () => {
+  const targets = fs.readdirSync(endToEndDir)
+
+  targets.forEach(target => {
+    if (targetOnly && target !== targetOnly) return
+
+    describe(target, () => {
+      const targetDir = path.join(endToEndDir, target)
+      const argsGeojson = load.sync(path.join(targetDir, 'args.geojson'))
+      const args = argsGeojson.features.map(f => f.geometry.coordinates)
+
+      possibleOps.forEach(op => {
+        if (opOnly && op !== opOnly) return
+
+        let resultGeojson
+        try {
+          resultGeojson = load.sync(path.join(targetDir, `${op}.geojson`))
+        } catch (err) {
+          /* missing result file indicates no need to test that operation */
+          return
+        }
+        test(op, () => {
+          const expected =
+            resultGeojson.geometry.coordinates.length > 0
+              ? resultGeojson.geometry.coordinates
+              : null
+          const result = polygonClipping[op](...args)
+          expect(result).toEqual(expected)
+        })
+      })
+    })
+  })
+})
