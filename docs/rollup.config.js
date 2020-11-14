6762ab75ce9434b44addb711081234a67a95a129
Mike Fogel <mike@fogel.ca>
2020-11-14 14:44:39 -0300

    chore: upgrade bunch of dev dependencies

--- docs/rollup.config.js
+++ docs/rollup.config.js
@@ -1,7 +1,7 @@
 import babel from 'rollup-plugin-babel'
-import commonjs from 'rollup-plugin-commonjs'
-import json from 'rollup-plugin-json'
-import resolve from 'rollup-plugin-node-resolve'
+import commonjs from '@rollup/plugin-commonjs'
+import json from '@rollup/plugin-json'
+import resolve from '@rollup/plugin-node-resolve'
 import { terser } from 'rollup-plugin-terser'
 import vue from 'rollup-plugin-vue'
 
