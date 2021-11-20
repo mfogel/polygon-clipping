6c426dfdba49d6ad96f55262c027653c0ac161af
z3dev <z3-dev@gfnews.net>
2021-11-20 16:47:53 +0900

    fix(polygon-clipping): corrected cmp of floating point numbers

--- src/flp.js
+++ src/flp.js
@@ -9,22 +9,14 @@ let epsilon = Number.EPSILON
 // IE Polyfill
 if (epsilon === undefined) epsilon = Math.pow(2, -52)
 
-const EPSILON_SQ = epsilon * epsilon
-
-/* FLP comparator */
+/**
+ * Floating point comparator.
+ * @param {Number} a - value
+ * @param {Number} b - value
+ * @returns {Number} 0 when value a and b are equal, -1 when value a < b, 1 when value a > b
+ */
 export const cmp = (a, b) => {
-  // check if they're both 0
-  if (-epsilon < a && a < epsilon) {
-    if (-epsilon < b && b < epsilon) {
-      return 0
-    }
-  }
-
-  // check if they're flp equal
-  const ab = a - b
-  if (ab * ab < EPSILON_SQ * a * b) {
-    return 0
-  }
+  if (Math.abs(a - b) < epsilon) return 0
 
   // normal comparison
   return a < b ? -1 : 1
